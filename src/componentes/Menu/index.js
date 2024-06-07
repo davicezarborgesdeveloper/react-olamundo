b4e3c0bc7e44eae7b3e6b07dde14900abe71fa7b
Davi Borges <borges.davi.cezar@gmail.com>
2024-06-07 16:40:27 -0300

    links

---
+++ src/componentes/Menu/index.js
@@ -0,0 +1,16 @@
+import { Link, useLocation } from 'react-router-dom'
+import styles from './Menu.module.css'
+import MenuLink from '../MenuLink';
+
+export default function Menu() {
+    const localizacao = useLocation();
+    return (
+        <header>
+            <nav className={styles.navegacao} >
+                <MenuLink to="/">Inicio</MenuLink>
+                <MenuLink to="/sobremim">Sobre Min</MenuLink>
+            </nav>
+
+        </header>
+    )
+}
