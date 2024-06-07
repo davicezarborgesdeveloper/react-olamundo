20f86bcb0ecd56449ebdef830309af06b9684f1b
Davi Borges <borges.davi.cezar@gmail.com>
2024-06-07 10:23:41 -0300

    roteamento

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import SobreMim from "./paginas/SobreMim";
 import Inicio from "./paginas/inicio";
 
-const pagina = window.location.pathname === '/' ? <Inicio /> : <SobreMim />
 function App() {
-  return pagina;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Inicio />} />
+        <Route path="/sobremim" element={<SobreMim />} />
+        <Route path="*" element={<div>Pagina não encontrada</div>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
