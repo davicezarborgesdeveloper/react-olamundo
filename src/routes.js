deeddd1c3d418a4cbbfea050aba44727991bde81
Davi Borges <borges.davi.cezar@gmail.com>
2024-06-11 16:03:00 -0300

    aninhando rotas

--- src/routes.js
+++ src/routes.js
@@ -3,14 +3,18 @@ import SobreMim from "./paginas/SobreMim";
 import Inicio from "./paginas/inicio";
 import Menu from "./componentes/Menu";
 import Rodape from "componentes/Rodape";
+import PaginaPadrao from "componentes/PaginaPadrao";
 
 function AppRoutes() {
   return (
     <BrowserRouter>
       <Menu />
       <Routes>
-        <Route path="/" element={<Inicio />} />
-        <Route path="/sobremim" element={<SobreMim />} />
+        <Route path='/' element={<PaginaPadrao />}>
+          <Route path="/" element={<Inicio />} />
+          <Route path="/sobremim" element={<SobreMim />} />
+        </Route>
+
         <Route path="*" element={<div>Pagina não encontrada</div>} />
       </Routes>
       <Rodape />
