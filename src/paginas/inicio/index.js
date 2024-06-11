16ba48bd91aca5daa046201e9b68f093cc4aa925
Davi Borges <borges.davi.cezar@gmail.com>
2024-06-11 17:03:57 -0300

    sobre Mim Completado

--- src/paginas/inicio/index.js
+++ src/paginas/inicio/index.js
@@ -1,6 +1,8 @@
 import styles from './Inicio.module.css';
+
 import posts from 'json/posts.json';
 import Post from "componentes/Post";
+
 export default function Inicio() {
     return (
         <ul className={styles.posts}>
@@ -8,8 +10,7 @@ export default function Inicio() {
                 <li key={post.id}>
                     <Post post={post} />
                 </li>
-            ))
-            }
+            ))}
         </ul>
     )
 }
--- src/routes.js
+++ src/routes.js
@@ -9,14 +9,16 @@ function AppRoutes() {
   return (
     <BrowserRouter>
       <Menu />
+
       <Routes>
-        <Route path='/' element={<PaginaPadrao />}>
-          <Route path="/" element={<Inicio />} />
-          <Route path="/sobremim" element={<SobreMim />} />
+        <Route path="/" element={<PaginaPadrao />}>
+          <Route index element={<Inicio />} />
+          <Route path="sobremim" element={<SobreMim />} />
         </Route>
 
-        <Route path="*" element={<div>Pagina não encontrada</div>} />
+        <Route path="*" element={<div>Página não encontrada</div>} />
       </Routes>
+
       <Rodape />
     </BrowserRouter>
   );
